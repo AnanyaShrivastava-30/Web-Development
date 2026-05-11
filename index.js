2908ba9fbf99a0159068f31f8d7c943cdec52e80
Ananya Shrivastava <shrivastavaananya324@gmail.com>
2026-05-11 13:10:35 +0530

    lect 1

---
+++ index.js
@@ -0,0 +1,7 @@
+//console.log("hiiiiihiiiihaaaahaaahaahiii");
+let btn = document.querySelector(".hi") //class
+//id
+let body = document.querySelector("body")
+btn.addEventListener("click",function(){
+    body.style.backgroundColor="pink"
+})
